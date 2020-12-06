249d0b38aee855a339ce3c546d06d48f64d6e54f
Thibault Deutsch <thibault.deutsch@gmail.com>
2020-12-06 18:09:59 +0000

    Add 2020/day4

---
+++ 2020/day4.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+
+required_fields = (
+    re.compile(r'byr:(19[2-9][0-9]|200[0-2])\s'),
+    re.compile(r'iyr:(201[0-9]|2020)\s'),
+    re.compile(r'eyr:(202[0-9]|2030)\s'),
+    re.compile(r'hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)\s'),
+    re.compile(r'hcl:#[0-9a-f]{6}\s'),
+    re.compile(r'ecl:(amb|blu|brn|gry|grn|hzl|oth)\s'),
+    re.compile(r'pid:[0-9]{9}\s')
+)
+
+
+def validate_passport(passport):
+    for field in required_fields:
+        if field.search(passport) is None:
+            return False
+
+    return True
+
+
+def main(input_file):
+    with open(input_file) as f:
+        lines = f.readlines()
+
+    start = 0
+    valid = 0
+    for i, line in enumerate(lines):
+        if line != '\n':
+            continue
+
+        valid += validate_passport(''.join(lines[start:i]))
+        start = i
+
+    valid += validate_passport(''.join(lines[start:]))
+    print(f'{valid=}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('usage: ./script.py [input-file]')
+        sys.exit(1)
+
+    main(sys.argv[1])
