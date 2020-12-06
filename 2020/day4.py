f8032904466e20d4626fbaefd937cf82bc075010
Thibault Deutsch <thibault.deutsch@gmail.com>
2020-12-06 20:52:57 +0000

    Simplify 2020/day4

--- 2020/day4.py
+++ 2020/day4.py
@@ -5,13 +5,13 @@ import sys
 
 
 required_fields = (
-    re.compile(r'byr:(19[2-9][0-9]|200[0-2])\s'),
-    re.compile(r'iyr:(201[0-9]|2020)\s'),
-    re.compile(r'eyr:(202[0-9]|2030)\s'),
-    re.compile(r'hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)\s'),
-    re.compile(r'hcl:#[0-9a-f]{6}\s'),
-    re.compile(r'ecl:(amb|blu|brn|gry|grn|hzl|oth)\s'),
-    re.compile(r'pid:[0-9]{9}\s')
+    re.compile(r'byr:(19[2-9][0-9]|200[0-2])(\s|$)'),
+    re.compile(r'iyr:(201[0-9]|2020)(\s|$)'),
+    re.compile(r'eyr:(202[0-9]|2030)(\s|$)'),
+    re.compile(r'hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)(\s|$)'),
+    re.compile(r'hcl:#[0-9a-f]{6}(\s|$)'),
+    re.compile(r'ecl:(amb|blu|brn|gry|grn|hzl|oth)(\s|$)'),
+    re.compile(r'pid:[0-9]{9}(\s|$)')
 )
 
 
@@ -19,24 +19,14 @@ def validate_passport(passport):
     for field in required_fields:
         if field.search(passport) is None:
             return False
-
     return True
 
 
 def main(input_file):
     with open(input_file) as f:
-        lines = f.readlines()
+        buf = f.read()
 
-    start = 0
-    valid = 0
-    for i, line in enumerate(lines):
-        if line != '\n':
-            continue
-
-        valid += validate_passport(''.join(lines[start:i]))
-        start = i
-
-    valid += validate_passport(''.join(lines[start:]))
+    valid = sum(validate_passport(p) for p in buf.split('\n\n'))
     print(f'{valid=}')
 
 
